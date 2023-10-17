c3b1867776336904595667631f2b22df97c29039
Marcjanna <marcjanna.bakowska@gmail.com>
2023-10-17 19:40:05 +0200

    added assets and type of plot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, no_update
 
 from utils import *
 
@@ -6,8 +6,8 @@ app = Dash(__name__)
 
 app.layout = html.Div(
     [
-        html.H1("title template"),
-        html.P("Wybierz zanieczyszczenie:"),
+        html.Div("title template", id="title", className="title"),
+        html.P("Wybierz zanieczyszczenie:", className="text"),
         dcc.Slider(min=2016, max=2020,
                    step=None,
                    marks={
@@ -18,45 +18,70 @@ app.layout = html.Div(
                        2020: '2020',
                    },
                    id='slider_year',
-                   value=2020
+                   value=2020,
+                   className="slider"
                    ),
-        dcc.RadioItems(
-            id="pollution",
-            options=load_pollution_names(),
-            value="co2",
-            inline=True,
+        html.Div(
+            [
+                dcc.Dropdown(
+                    id="pollution",
+                    options=load_pollution_names(),
+                    placeholder="Select a pollution",
+                    className="dropdown"
+                ),
+                dcc.RadioItems(
+                    id="type_of_plotting",
+                    options=["per km square", "overall"],
+                    inline=True,
+                    value="per km square",
+                    className="radio"
+                ),
+            ],
+            className="row",
         ),
-        dcc.Graph(id="graph"),
+        dcc.Graph(id="graph", className="graph", figure=blank_fig()),
     ]
 )
+
 @app.callback(
     Output("graph", "figure"),
     Input("pollution", "value"),
     Input("slider_year", "value"),
+    Input("type_of_plotting", "value")
 )
-def map_plot(pollution, slider_year):
-    url = "data/pollution_coordinates.geojson"
+def map_plot(pollution, slider_year, type_of_plotting):
+    try:
+        url = "data/pollution_coordinates.geojson"
+        districts = gpd.read_file(url)
 
-    districts = gpd.read_file(url)
+        if type_of_plotting == "overall":
+            data_name = (pollution + "_" + str(slider_year))
+        elif type_of_plotting == "per km square":
+            data_name = (pollution + "_" + str(slider_year) + "_per_km_sq")
+        else:
+            return no_update
 
-    data_name = (pollution+"_"+str(slider_year))
+        fig = px.choropleth(
+            districts,
+            geojson=districts.geometry,
+            locations="id",
+            color=data_name,
+            projection="mercator",
+            color_continuous_scale='RdYlGn_r',
+            hover_name="nazwa",
+            hover_data={data_name: True, "id": False},
+            labels={data_name: ""},
+        )
+        fig.update_geos(fitbounds="locations", visible=False)
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        )
 
-    fig = px.choropleth(
-        districts,
-        geojson=districts.geometry,
-        locations="id",
-        color=data_name,
-        projection="mercator",
-        color_continuous_scale = 'RdYlGn_r',
-        hover_name="nazwa",
-        hover_data={data_name: True, "id": False},
-    )
-    fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+        return fig
 
-    return fig
+    except:
+        return no_update
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
     # init_data()
+    app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,31 +1,68 @@
 import geopandas as gpd
+import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
 import pandas as pd
+from numpyencoder import NumpyEncoder
 import json
+import plotly.graph_objects as go
 
 def init_data():
     with open('data/wojewodztwa-min.geojson', 'r') as f:
         data_geojson = json.load(f)
+    data = pd.ExcelFile(r"data/pollution_data.xlsx")
+
+    add_area(data_geojson, data)
 
     for year in ['2020', '2019', '2018', '2017', '2016']:
-
-        data = pd.ExcelFile(r"data/pollution_data.xlsx")
-
         df1 = pd.read_excel(data, 'data'+year)
         df = pd.DataFrame(df1)
 
         for feat in data_geojson['features']:
             for woj in range(len(df["woj."])):
                 if feat["properties"]["nazwa"] == df["woj."][woj]:
-                    for pol in list(df.columns)[1:]:
-                        feat['properties'][str(pol)+"_"+year] = df[pol][woj]
+                    for pol in list(df.columns)[2:]:
+                        feat['properties'][str(pol) + "_" + year] = df[pol][woj]
+
+    add_data_per_km2(data_geojson, data)
 
     with open('data/pollution_coordinates.geojson', 'w') as f:
-        json.dump(data_geojson, f)
+        json.dump(data_geojson, f, cls=NumpyEncoder)
+
+
+def add_area(data_geojson, data):
+    df1 = pd.read_excel(data, 'data2020')
+    df = pd.DataFrame(df1)
+
+    for feat in data_geojson['features']:
+        for woj in range(len(df["woj."])):
+            if feat["properties"]["nazwa"] == df["woj."][woj]:
+                feat['properties']['area'] = df['area'][woj]
+
+
+def add_data_per_km2(data_geojson, data):
+    for year in ['2020', '2019', '2018', '2017', '2016']:
+        df1 = pd.read_excel(data, 'data'+year)
+        df = pd.DataFrame(df1)
+
+        for feat in data_geojson['features']:
+            for woj in range(len(df["woj."])):
+                if feat["properties"]["nazwa"] == df["woj."][woj]:
+                    for pol in list(df.columns)[2:]:
+                        feat['properties'][str(pol) + "_" + year + "_per_km_sq"] = df[pol][woj]/df['area'][woj]
+
 
 def load_pollution_names():
     data = pd.read_excel(r"data\pollution_data.xlsx")
     df = pd.DataFrame(data)
 
-    return [pol for pol in list(df.columns)[1:]]
+    return [pol for pol in list(df.columns)[2:]]
+
+
+def blank_fig():
+    fig = go.Figure(go.Scatter(x=[], y=[]))
+    fig.update_layout(template=None)
+    fig.update_xaxes(showgrid=False, showticklabels=False, zeroline=False)
+    fig.update_yaxes(showgrid=False, showticklabels=False, zeroline=False)
+
+    return fig
